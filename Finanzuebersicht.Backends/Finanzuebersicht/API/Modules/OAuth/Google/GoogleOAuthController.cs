1f4c7e97be6c8f9adba9eb31b4b418c4ed1a8f3f
Lukas Wessel <wessel5991@gmail.com>
2023-08-27 18:37:34 +0200

    google oauth works i guess

--- Finanzuebersicht.Backends/Finanzuebersicht/API/Modules/OAuth/Google/GoogleOAuthController.cs
+++ Finanzuebersicht.Backends/Finanzuebersicht/API/Modules/OAuth/Google/GoogleOAuthController.cs
@@ -1,36 +1,17 @@
-﻿using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.Google;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Finanzuebersicht.API.Modules.OAuth.Google
 {
-    [Route("account")]
+    [Route("myuserapp")]
 
     public class AuthController : Controller
     {
-        [HttpGet("signin-google")]
-        public IActionResult SignInGoogle()
+        [HttpGet("{id}")]
+        [Authorize]
+        public ActionResult<string> Get(int id)
         {
-            var authProperties = new AuthenticationProperties
-            {
-                RedirectUri = Url.Action("GoogleResponse")
-            };
-            return Challenge(authProperties, GoogleDefaults.AuthenticationScheme);
-        }
-
-        [HttpGet("google-response")]
-        public async Task<IActionResult> GoogleResponse()
-        {
-            var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-
-            if (result?.Succeeded != true)
-            {
-                return BadRequest();
-            }
-
-            var x = User;
-            return Ok();
+            return this.User.Identity.Name;
         }
     }
 }
