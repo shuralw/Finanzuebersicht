f99ce308525cdb8cd10a086e6885cf718d9cc443
Lukas Wessel <wessel5991@gmail.com>
2023-08-27 16:03:15 +0200

    now other try

---
+++ Finanzuebersicht.Backends/Finanzuebersicht/API/Modules/OAuth/Google/GoogleOAuthController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Finanzuebersicht.API.Modules.OAuth.Google
+{
+    [Route("account")]
+    public class AccountController : Controller
+    {
+        [HttpGet("login-with-google")]
+        public IActionResult LoginWithGoogle()
+        {
+            // You can add your post-authentication redirect URL here
+            string redirectUrl = Url.Action("GoogleResponse", "Account");
+
+            var properties = new AuthenticationProperties { RedirectUri = redirectUrl };
+
+            return Challenge(properties, "Google");
+        }
+
+        [HttpGet("google-response")]
+        public async Task<IActionResult> GoogleResponse()
+        {
+            // Extract the user's info from the authentication response
+            var result = await HttpContext.AuthenticateAsync("Cookies");
+
+            if (result?.Succeeded != true)
+            {
+                return BadRequest("Authentication failed");
+            }
+
+            var claimsIdentity = (ClaimsIdentity)result.Principal.Identity;
+            var emailClaim = claimsIdentity.FindFirst(ClaimTypes.Email);
+
+            // Perform your business logic here, for example, log the email or save to DB
+            // string email = emailClaim.Value;
+
+            return Ok("Successfully authenticated");
+        }
+    }
+}
--- Finanzuebersicht.Backends/Finanzuebersicht/Program.cs
+++ Finanzuebersicht.Backends/Finanzuebersicht/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.OAuth;
 
 namespace Finanzuebersicht
 {
@@ -6,11 +7,48 @@ namespace Finanzuebersicht
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var configuration = builder.Configuration;
 
             // Add services to the container.
 
             builder.Services.AddControllers();
-            builder.Services.AddAuthentication(); // Sample, you'll replace this with your actual OAuth setup.
+
+            builder.Services.AddDistributedMemoryCache();
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(20);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+            Console.WriteLine($"Client ID: {configuration["Authentication:Google:ClientId"]}");
+            Console.WriteLine($"Client Secret: {configuration["Authentication:Google:ClientSecret"]}");
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "Cookies";
+                options.DefaultChallengeScheme = "Google";
+            })
+            .AddCookie("Cookies")
+            .AddGoogle("Google", options =>
+            {
+                options.ClientId = configuration["Authentication:Google:ClientId"];
+                options.ClientSecret = configuration["Authentication:Google:ClientSecret"];
+                options.CallbackPath = new PathString("/signin-google");
+
+                options.Events = new OAuthEvents
+                {
+                    OnRedirectToAuthorizationEndpoint = context =>
+                    {
+                        Console.WriteLine($"Redirecting to Authorization Endpoint: {context.RedirectUri}");
+                        return Task.CompletedTask;
+                    },
+                    OnCreatingTicket = context =>
+                    {
+                        Console.WriteLine("Creating OAuth ticket");
+                        return Task.CompletedTask;
+                    }
+                };
+            });
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
@@ -27,7 +65,7 @@ namespace Finanzuebersicht
 
             app.UseHttpsRedirection();
 
-
+            app.UseSession();
             app.UseRouting();
             app.UseAuthentication();
             app.UseAuthorization();
